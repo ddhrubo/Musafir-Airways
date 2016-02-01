6cf90256c534c11180249e5bfceabb9cab43588c
Dhrubo62 <mohiuddin.fahim49@gmail.com>
2016-02-01 23:03:38 +0600

    Kick Start!

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title>Greetings from Musafir Airways</title>
+		<style type="text/css">
+			
+			/* cover image section of homepage */
+			#cover {
+
+			}
+			#cover_image {
+				width: 100%;
+			}
+			
+		</style>
+	</head>
+
+	<body>
+		<?php
+			require "header.php";
+		?>
+		
+		<!-- Cover image section -->
+		<div id="cover">
+			<img id="cover_image" src="img/index/img1.jpg">
+		</div>
+
+		<!-- Page heading section -->
+		<div id="page_heading" >
+			Home
+		</div>
+
+	</body>
+</html>
